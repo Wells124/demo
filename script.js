12fb7a456a2d104d41e7086aa883dfb682056e82
Wells124 <107061191+Wells124@users.noreply.github.com>
2025-05-24 14:30:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("YouTube Clone Loaded");
+
+document.querySelector('input[type="text"]').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    alert("Search not implemented in static version.");
+  }
+});
